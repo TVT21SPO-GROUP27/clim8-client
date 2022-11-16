9822cadcc38bdcb6943f574166289a619a091eab
nikkedoy98 <niklas.siltala@gmail.com>
2022-11-16 16:02:56 +0200

    :sparkles: | Add scaling and some basic functionality multiple datasets.

--- src/components/EkaVisualisointi.js
+++ src/components/EkaVisualisointi.js
@@ -1,3 +1,4 @@
+import zoomPlugin from 'chartjs-plugin-zoom';
 import {useEffect, useState} from 'react';
 import {
     Chart as ChartJS,
@@ -24,6 +25,60 @@ ChartJS.register(
     Legend
   );
 
+ChartJS.register(
+    LineElement,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    Title,
+    Tooltip,
+    Filler,
+    Legend,
+    zoomPlugin
+  );
+
+function getCorrectSummarySeries(summarySeries) {
+  if(summarySeries === "HADCRUT_GLOBAL") return "global";
+  else if(summarySeries === "HADCRUT_NORTHERN_HEMISPHERE") return "northern";
+  else return "southern";
+}
+
+const config = {
+  scales: {
+    x: {
+      min: 1,
+      max: 100,
+      suggestedMax: 40,
+      suggestedMin: 1
+    },
+    y: {
+      min: -1,
+      max: 1,
+      beginAtZero: true
+    }
+  },
+  plugins: {
+    zoom: {
+      zoom: {
+        wheel: {
+          enabled: true
+        },
+        mode: "xy",
+        speed: 100
+      },
+      pan: {
+        enabled: true,
+        mode: "xy",
+        speed: 100
+      }
+    },
+    title: {
+      display: true,
+      text: 'Ilmasto 1850-2022'
+    }
+  }
+}
+
 export default function EkaVisualisointi() {
     const [data, setData] = useState({
         labels:'Ilmasto 1850-2022',
@@ -34,37 +89,69 @@ export default function EkaVisualisointi() {
             borderColor: 'rgb(255, 99, 132)',
             backgroundColor: 'rgba(25, 90, 13, 0.5)',
           },
-        ],
+        ]
       });
     useEffect(()=> {
        const fetchData= async()=> {
-           const url = 'http://localhost:8080/hadcrutdata'
-           const temp = [];
-           const year = [];
+          const url = 'http://localhost:8080/hadcrutdata'
+
+          let anual = new Map([
+            ["global", new Map()],
+            ["northern", new Map()],
+            ["southern", new Map()]
+          ]);
+
+          let monthly = new Map([
+            ["global", new Map()],
+            ["northern", new Map()],
+            ["southern", new Map()]
+          ]);
          await fetch(url).then((data)=> {
-             console.log(data)
-             const res = data.json();
-             return res
+            console.log(data)
+            const res = data.json();
+            return res
          }).then((res) => {
-             console.log(res)
+            console.log(res)
             for (const val of res) {
-                temp.push(val.data);
-                year.push(val.year)
+              if(val.month === 0) {
+                console.log("Found data for anual!");
+                anual.get(getCorrectSummarySeries(val.summarySeries)).set(val.year, val.data);
+              } else {
+                console.log("Found data for montly!");
+                monthly.get(getCorrectSummarySeries(val.summarySeries)).set(val.month + "/" + val.year, val.data);
+              }
             }
+
             setData({
-                labels:year,
+                labels: Array.from(monthly.get("global").keys()),
                 datasets: [
                   {
-                    label: 'Dataset',
-                    data:temp,
+                    label: 'Monthly - Global',
+                    data: Array.from(monthly.get("global").values()),
                     fill: false,
                     borderColor: 'rgb(75, 192, 192)',
                     tension: 0.1,
                     indexAxis: 'x'
+                  },
+                  {
+                    label: 'Monthly - Northern',
+                    data: Array.from(monthly.get("northern").values()),
+                    fill: false,
+                    borderColor: 'rgb(0, 234, 255)',
+                    tension: 0.1,
+                    indexAxis: 'x'
+                  },
+                  {
+                    label: 'Monthly - Southern',
+                    data: Array.from(monthly.get("southern").values()),
+                    fill: false,
+                    borderColor: 'rgb(0, 0, 255)',
+                    tension: 0.1,
+                    indexAxis: 'x'
                   }
-                ],
+                ]
               })
-            console.log(temp, year)
+            //console.log(temp, year)
          }).catch(e => {
                 console.log("error", e)
             })
@@ -74,8 +161,8 @@ export default function EkaVisualisointi() {
     },[])
    
     return(
-        <div style={{width:'80%', height:'50%'}}>
-            <div><Line data={data}/></div>
+        <div style={{width:'50%', height:'10%'}}>
+            <div><Line options={config} data={data}/></div>
         </div>
          )
          /*
